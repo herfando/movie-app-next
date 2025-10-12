6114053fb4067aa878291067222a79f0e75f6c78
Herfando <herfando_91@yahoo.com>
2025-10-12 16:59:54 +0700

    edit navbar

--- src/components/container/home.tsx
+++ src/components/container/home.tsx
@@ -7,24 +7,29 @@ export default function home() {
       <div className="relative w-full overflow-hidden">
         <img className="w-full absolute -z-10" src="/image1.png" alt="imageawal" />
         {/* Navigation */}
-        <nav className=" px-20 w-full sm:h-[90px] h-[64px] mx-auto grid grid-cols-3 justify-between items-center">
-          <div className="flex flex-1 gap-3 justify-start items-center">
+        <nav className="bg-amber-300 w-full sm:h-[90px] h-[64px] mx-auto grid grid-cols-2 sm:grid-cols-3 justify-between items-center">
+          {/* Movie */}
+          <div className=" bg-amber-800 flex flex-1 gap-3 justify-start items-center">
             <img className="sm:w-[33.33px] sm:h-[31.18] w-[23.33px] h-[21.82px]" src="/Vector1.png" alt="logo movie" />
             <h1 className="sm:text-[28.4px] text-[19.9px]">Movie</h1>
           </div>
-          <h2 className="sm:flex flex-1 gap-10 hidden font-medium">
+          {/* Middle list */}
+          <h2 className="bg-red-400 sm:flex flex-1 gap-10 hidden font-medium">
             <span>Home</span> <span>Favorites</span>
           </h2>
-          <form action="" className="relative sm:flex hidden">
+          {/* Search & Menu */}
+          <form action="" className="bg-amber-800 relative sm:flex hidden items-center">
             <img src="/Vector2.png" alt="search" className="absolute" />
-            <input type="text" placeholder="Search movie" className="border border-black rounded-md w-[243px] h-[56]" />
+            <input type="text" placeholder="Search movie" className="border border-black rounded-md w-[243px] h-[56] pl-6" />
           </form>
-          <img src="/Vector3.png" alt="search" className="sm:hidden flex"/>
-
+          <div className="bg-amber-700 sm:hidden flex flex-1 justify-end items-center gap-4">
+            <img src="/Vector3.png" alt="search" />
+            <img src="/Menu1.png" alt="search" />
+          </div>
         </nav>
 
         {/* Hero */}
-        <section className="w-full sm:h-[673px] h-[448px]">selmat pagi
+        <section className="w-full sm:h-[673px] h-[448px]">
 
         </section>
       </div>
