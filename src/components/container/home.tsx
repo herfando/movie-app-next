c0daa897c875d268e63f14dcdd8ea54e99e37aaa
Herfando <herfando_91@yahoo.com>
2025-10-13 21:15:55 +0700

    create new release

--- src/components/container/home.tsx
+++ src/components/container/home.tsx
@@ -10,7 +10,7 @@ export interface Movie {
   rating: string;
 }
 
-const movie: Movie[] = [
+const trendingmovie: Movie[] = [
   { id: 1, img: "/movie1.png", title:"The Gorge", rating: "7.9" },
   { id: 2, img: "/movie2.png", title:"Mufasa: The Lion King", rating: "7.9" },
   { id: 3, img: "/movie3.png", title:"The Brutalist", rating: "7.9" },
@@ -18,6 +18,27 @@ const movie: Movie[] = [
   { id: 5, img: "/movie5.png", title:"Brave New World", rating: "7.9" },
   { id: 6, img: "/movie6.png", title:"Dog Man", rating: "7.0" },
 ];
+
+const newmovie: Movie[] = [
+  { id: 7, img: "/movie7.png", title:"Conclave", rating: "7.1" },
+  { id: 8, img: "/movie8.png", title:"Flight Risk", rating: "5.8" },
+  { id: 9, img: "/movie9.png", title:"Paddington in Peru", rating: "7.0" },
+  { id: 10, img: "/movie10.png", title:"My Fault: London", rating: "7.67" },
+  { id: 11, img: "/movie11.png", title:"Mad About the Boy", rating: "6.8" },
+  { id: 12, img: "/movie12.png", title:"Better Man", rating: "7.5" },
+  { id: 13, img: "/movie13.png", title:"Sirens of the Deep", rating: "7.3" },
+  { id: 14, img: "/movie14.png", title:"Nosferatu", rating: "6.7" },
+  { id: 15, img: "/movie15.png", title:"September 5", rating: "6.9" },
+  { id: 16, img: "/movie16.png", title:"Anora", rating: "7.0" },
+  { id: 17, img: "/movie17.png", title:"Ne Zha 2", rating: "7.8" },
+  { id: 18, img: "/movie18.png", title:"The Last Showgirl", rating: "6.7" },
+  { id: 19, img: "/movie19.png", title:"Sonic the Hedgehog 3", rating: "7.7" },
+  { id: 20, img: "/movie20.png", title:"La Dolce Villa", rating: "6.4" },
+  { id: 21, img: "/movie21.png", title:"The Order", rating: "6.6" },
+];
+
+
+
 
 
 export default function home() {
@@ -27,9 +48,9 @@ export default function home() {
         <img className="w-full absolute -z-10 sm:flex hidden" src="/image1.png" alt="image1" />
         <img className="w-full absolute -z-10 sm:hidden flex" src="/image2.png" alt="image2" />
         {/* Navigation */}
-        <nav className="  w-full sm:h-[90px] h-[64px] mx-auto grid sm:grid-cols-[1fr_3fr_1fr] grid-cols-2 justify-around items-center">
+        <nav className="w-full sm:h-[90px] h-[64px] mx-auto grid sm:grid-cols-[1fr_3fr_1fr] grid-cols-2 justify-around items-center">
           {/* Movie */}
-          <div className="text-[#FDFDFD] translate-x-3 sm:translate-x-0  flex flex-1 gap-3 sm:justify-center justify-start items-center">
+          <div className="sm:translate-x-20 translate-x-5 text-[#FDFDFD] flex flex-1 gap-3 items-center">
             <img className="sm:w-[33.33px] sm:h-[31.18] w-[23.33px] h-[21.82px]" src="/Vector1.png" alt="logo movie" />
             <h1 className="sm:text-[28.4px] text-[19.9px]">Movie</h1>
           </div>
@@ -51,7 +72,7 @@ export default function home() {
         {/* Hero */}
         <section className="w-full sm:h-[673px] h-[448px] flex items-center">
           {/* Text */}
-          <div className="translate-x-3 mr-4 translate-y-[60px] sm:translate-y-0 sm:translate-x-15 pl-0 sm:w-[635px] w-full grid sm:gap-7 gap-2">
+          <div className="translate-x-3 mr-4 translate-y-[60px] sm:translate-y-0 sm:translate-x-20 pl-0 sm:w-[635px] w-full grid sm:gap-7 gap-2">
             <h1 className="text-2xl sm:text-5xl font-bold text-[#FDFDFD]">The Gorge</h1>
             <h4 className="text-md font-medium text-[#A4A7AE]">Two highly trained operatives grow close from a distance after being sent to guard opposite sides of a mysterious gorge. When an evil below emerges, they must work together to survive what lies within.</h4>
             {/* Button */}
@@ -69,12 +90,11 @@ export default function home() {
       </div>
 
       {/* Trending Now */}
-      <section className="w-full py-10 px-[60px] bg-black">
+      <section className="w-full py-10 sm:px-[80px] px-[50px] bg-black">
         <h1 className="sm:text-5xl text-2xl mb-6 font-bold text-white">Trending Now</h1>
-
         <Carousel className="w-full mx-auto ">
           <CarouselContent>
-            {movie.map((item) => (
+            {trendingmovie.map((item) => (
               <CarouselItem
                 key={item.id}
                 className="basis-1/2 sm:basis-1/3 lg:basis-1/5"
@@ -93,7 +113,6 @@ export default function home() {
               </CarouselItem>
             ))}
           </CarouselContent>
-
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
@@ -101,13 +120,27 @@ export default function home() {
 
 
       {/* New Release */}
-      <section>
+      <section className="w-full py-10 sm:px-[80px] px-[50px] bg-black">
+        <h1 className="sm:text-5xl text-2xl mb-6 font-bold text-white">
+          New Release
+        </h1>
 
-
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+          {newmovie.map((item) => (
+            <div key={item.id} className="flex flex-col items-center">
+              <img
+                src={item.img}
+                alt={item.title}
+                width={216}
+                height={321}
+                className="rounded-xl shadow-lg hover:scale-105 transition-all duration-300"
+              />
+              <h2 className="text-lg font-semibold mt-3 text-[#FDFDFD]">{item.title}</h2>
+              <p className="text-sm text-gray-400">⭐ {item.rating}/10</p>
+            </div>
+          ))}
+        </div>
       </section>
-
-
-
     </>
   )
 }
