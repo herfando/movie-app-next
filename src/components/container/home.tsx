c8b1a78f34bbe746d65f9b6e528088e03bb37b86
Herfando <herfando_91@yahoo.com>
2025-10-13 13:14:32 +0700

    edit hore mobile

--- src/components/container/home.tsx
+++ src/components/container/home.tsx
@@ -10,7 +10,7 @@ export default function home() {
         {/* Navigation */}
         <nav className="  w-full sm:h-[90px] h-[64px] mx-auto grid sm:grid-cols-[1fr_3fr_1fr] grid-cols-2 justify-around items-center">
           {/* Movie */}
-          <div className="translate-x-3 sm:translate-x-0  flex flex-1 gap-3 sm:justify-center justify-start items-center">
+          <div className="text-[#FDFDFD] translate-x-3 sm:translate-x-0  flex flex-1 gap-3 sm:justify-center justify-start items-center">
             <img className="sm:w-[33.33px] sm:h-[31.18] w-[23.33px] h-[21.82px]" src="/Vector1.png" alt="logo movie" />
             <h1 className="sm:text-[28.4px] text-[19.9px]">Movie</h1>
           </div>
@@ -32,16 +32,16 @@ export default function home() {
         {/* Hero */}
         <section className="w-full sm:h-[673px] h-[448px] flex items-center">
           {/* Text */}
-          <div className="sm:translate-y-[-40px] sm:pl-15 pl-0 sm:w-[635px] w-full grid gap-7">
-            <h1 className="text-5xl font-bold text-[#FDFDFD]">The Gorge</h1>
+          <div className="translate-x-3 mr-4 translate-y-[60px] sm:translate-y-0 sm:translate-x-15 pl-0 sm:w-[635px] w-full grid sm:gap-7 gap-2">
+            <h1 className="text-2xl sm:text-5xl font-bold text-[#FDFDFD]">The Gorge</h1>
             <h4 className="text-md font-medium text-[#A4A7AE]">Two highly trained operatives grow close from a distance after being sent to guard opposite sides of a mysterious gorge. When an evil below emerges, they must work together to survive what lies within.</h4>
             {/* Button */}
-            <div className="flex gap-5 sm:mt-5 text-[#FDFDFD]">
+            <div className="grid sm:grid-cols-2 grid-cols-1 sm:mt-5 text-[#FDFDFD]">
               <form action="" className="flex justify-center items-center sm:h-[52px] sm:w-[230px] relative rounded-full bg-[#961200]">
                 <button type="submit">Watch Trailer</button>
                 <img src="/Play.png" alt="vektor" className="pl-2" />
               </form>
-              <form action="" className="flex justify-center items-center sm:h-[52px] sm:w-[230px] bg-[#181D27] rounded-full">
+              <form action="" className="sm:translate-x-[-60px] flex justify-center items-center sm:h-[52px] sm:w-[230px] bg-[#181D27] rounded-full">
                 <button type="submit">See Detail</button>
               </form>
             </div>
