7176626c6fbd08199823b580d2c625415b9bdbf4
Herfando <herfando_91@yahoo.com>
2025-10-17 16:04:38 +0700

    edit mobile version favorite

--- src/components/container/Favorite.tsx
+++ src/components/container/Favorite.tsx
@@ -5,35 +5,45 @@ import { Heart, Calendar, Star, Video } from "lucide-react"
 export default function Favorite() {
   return (
       <section className="bg-black w-full md:h-[1383px] h-[1345px] flex items-center relative overflow-hidden">
-        <Image className=" top-0 left-0 w-full absolute" src="/image3.png" alt="background" width={1920} height={1080} />
-
+        <Image className="md:flex hidden top-0 left-0 w-full absolute" src="/image3.png" alt="background" width={1920} height={1080} />
+        <Image className="md:hidden flex top-0 left-0 w-full absolute" src="/image10.png" alt="background" width={1920} height={1080} />
         {/* Movie */}
-        <div className="absolute flex md:translate-x-20 translate-x-5 gap-10 -translate-y-30">
-          <div>
+        <div className="absolute flex md:translate-x-20 translate-x-5 gap-10 md:-translate-y-30 -translate-y-70">
+          <div className="absolute">
             <Image 
             className="md:w-[260px] md:h-[384px]" 
             src="/movie5.png" 
             alt="movie" 
-            width={116} 
+            width={126} 
             height={171} 
             />
           </div>
-          <div>
-            <div className="gap-5 grid content-start">
-              <span className="text-white text-4xl font-bold">Captain America: Brave New World</span>
-              <div className="items-center flex gap-2 text-2xl"><Calendar className="stroke-white text-white"/> <span className="text-white">12 Februari 2025</span></div>
+          <div className="grid grid-cols-1">
+            <div className="gap-5 grid content-start md:translate-x-80 translate-x-35">
+              <span className="text-white w-[229px] md:w-full md:text-4xl text-xl font-bold">Captain America: Brave New World</span>
+              <div className="items-center flex md:gap-2 gap-1 text-xs content-center"><Calendar className="stroke-white text-white md:scale-100 scale-75"/> <span className="text-white">12 Februari 2025</span></div>
+              {/* Watch Trailer */}
               <div className="flex items-center  gap-3">
-              <form className="text-white h-[52px] w-[220px] flex justify-center items-center md:h-[52px] md:w-[230px] relative rounded-full bg-[#961200]">
+              <form className="md:flex hidden text-white h-[52px] w-[220px] justify-center items-center md:h-[52px] md:w-[230px] relative rounded-full bg-[#961200]">
                 <button type="submit">Watch Trailer</button>
                 <Image src="/Play.png" alt="vektor" className="pl-2" width={30} height={30} />
               </form>
-              <div className="flex content-center md:w-[52px] md:h-[52px] w-[44px] h-[44px] border rounded-full justify-center items-center border-[#181D27] bg-[#181D27]"><Heart className="w-[24px] h-[24px] stroke-white fill-none md:stroke-[#961200] md:fill-[#961200]"></Heart></div>
+              <div className="md:flex hidden content-center md:w-[52px] md:h-[52px] w-[44px] h-[44px] border rounded-full justify-center items-center border-[#181D27] bg-[#181D27]"><Heart className="w-[24px] h-[24px] stroke-white fill-none md:stroke-[#961200] md:fill-[#961200]"></Heart></div>
               </div>
             </div>
-            <div className="text-white gap-10 flex mt-5">
-              <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Star className="stroke-[#E4A802] fill-[#E4A802]"/>Rating <span>6.2/10</span> </div>
-              <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Video className="stroke-white fill-white"/>Genre <span>Action</span> </div>
-              <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Image src="/Vector4.png" alt="agelimit" width={26.65} height={26.67}/>Age Limit<span>13</span> </div>       
+            {/* Watch Trailer */}
+            <div className="flex items-center gap-3 translate-y-20">
+              <form className="md:hidden flex text-white h-[44px] w-[250px] justify-center items-center md:h-[52px] md:w-[230px] relative rounded-full bg-[#961200]">
+                <button type="submit">Watch Trailer</button>
+                  <Image src="/Play.png" alt="vektor" className="pl-2" width={30} height={30} />
+                </form>
+                <div className="md:hidden flex content-center md:w-[52px] md:h-[52px] w-[44px] h-[44px] border rounded-full justify-center items-center border-[#181D27] bg-[#181D27]"><Heart className="w-[24px] h-[24px] stroke-white fill-none md:stroke-[#961200] md:fill-[#961200]"></Heart></div>
+            </div>
+
+            <div className=" text-white gap-10 flex mt-5 translate-x-80">
+              <div className="rounded-2xl flex flex-col gap-2 content-center md:w-[276px] md:h-[146px] w-[112px] h-[120px] bg-[#000000] justify-center items-center"> <Star className="stroke-[#E4A802] fill-[#E4A802]"/>Rating <span>6.2/10</span> </div>
+              <div className="rounded-2xl flex flex-col gap-2 content-center md:w-[276px] md:h-[146px] w-[112px] h-[120px] bg-[#000000] justify-center items-center"> <Video className="stroke-white fill-white"/>Genre <span>Action</span> </div>
+              <div className="rounded-2xl flex flex-col gap-2 content-center md:w-[276px] md:h-[146px] w-[112px] h-[120px] bg-[#000000] justify-center items-center"> <Image src="/Vector4.png" alt="agelimit" width={26.65} height={26.67}/>Age Limit<span>13</span> </div>       
             </div>
           </div>
         </div>
