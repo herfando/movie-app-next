22495b137a2ae3f133d28a6abbb3e831f9d75575
Herfando <herfando_91@yahoo.com>
2025-10-17 13:31:49 +0700

    create overview in favorite

--- src/components/container/Favorite.tsx
+++ src/components/container/Favorite.tsx
@@ -5,9 +5,10 @@ import { Heart, Calendar, Star, Video } from "lucide-react"
 export default function Favorite() {
   return (
       <section className="bg-black w-full md:h-[1383px] h-[1345px] flex items-center relative overflow-hidden">
-        <Image className="top-0 left-0 w-full absolute" src="/image3.png" alt="background" width={1920} height={1080} />
+        <Image className=" top-0 left-0 w-full absolute" src="/image3.png" alt="background" width={1920} height={1080} />
 
-        <div className="relative flex md:translate-x-20 translate-x-5 gap-5 -translate-y-30">
+        {/* Movie */}
+        <div className="absolute flex md:translate-x-20 translate-x-5 gap-5 -translate-y-30">
           <div>
             <Image 
             className="md:w-[260px] md:h-[384px]" 
@@ -22,7 +23,7 @@ export default function Favorite() {
               <span className="text-white text-4xl font-bold">Captain America: Brave New World</span>
               <div className="items-center flex gap-2 text-2xl"><Calendar className="stroke-white text-white"/> <span className="text-white">12 Februari 2025</span></div>
               <div className="flex items-center  gap-3">
-              <form className="h-[52px] w-[220px] flex justify-center items-center md:h-[52px] md:w-[230px] relative rounded-full bg-[#961200]">
+              <form className="text-white h-[52px] w-[220px] flex justify-center items-center md:h-[52px] md:w-[230px] relative rounded-full bg-[#961200]">
                 <button type="submit">Watch Trailer</button>
                 <Image src="/Play.png" alt="vektor" className="pl-2" width={30} height={30} />
               </form>
@@ -32,14 +33,22 @@ export default function Favorite() {
             <div className="text-white gap-5 flex mt-5">
               <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Star className="stroke-[#E4A802] fill-[#E4A802]"/>Rating <span>6.2/10</span> </div>
               <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Video className="stroke-white fill-white"/>Genre <span>Action</span> </div>
-              <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Image src="/Vector4.png" alt="agelimit" width={26.65} height={26.67}/>Age Limit<span>13</span> </div>
-              
-
-              
+              <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Image src="/Vector4.png" alt="agelimit" width={26.65} height={26.67}/>Age Limit<span>13</span> </div>       
             </div>
           </div>
         </div>
-      
+        
+        {/* Overview */}
+        <div className="absolute translate-y-45 md:translate-x-20 translate-x-5 mr-40 ">
+          <h1 className="text-4xl text-white font-bold mb-4">Overview</h1>
+          <span className="text-[#A4A7AE] text-l">After meeting with newly elected U.S. President Thaddeus Ross, Sam finds himself in the middle of an international incident. He must discover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.</span>
+        </div>
+
+        {/* Overview */}
+        <div className="absolute translate-y-80  md:translate-x-20 translate-x-5">
+          <h1 className="text-4xl text-white font-bold mb-4">Cast & Crew</h1>
+          <span className="text-[#A4A7AE] text-l">After meeting with newly elected U.S. President Thaddeus Ross, Sam finds himself in the middle of an international incident. He must discover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.</span>
+        </div>
       </section>
         );
       }
