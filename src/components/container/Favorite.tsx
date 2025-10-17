48561135a0e9bcaa194825e5407f8d4cc9eb7dd9
Herfando <herfando_91@yahoo.com>
2025-10-17 14:39:14 +0700

    edit styling favorite

--- src/components/container/Favorite.tsx
+++ src/components/container/Favorite.tsx
@@ -8,7 +8,7 @@ export default function Favorite() {
         <Image className=" top-0 left-0 w-full absolute" src="/image3.png" alt="background" width={1920} height={1080} />
 
         {/* Movie */}
-        <div className="absolute flex md:translate-x-20 translate-x-5 gap-5 -translate-y-30">
+        <div className="absolute flex md:translate-x-20 translate-x-5 gap-10 -translate-y-30">
           <div>
             <Image 
             className="md:w-[260px] md:h-[384px]" 
@@ -30,7 +30,7 @@ export default function Favorite() {
               <div className="flex content-center md:w-[52px] md:h-[52px] w-[44px] h-[44px] border rounded-full justify-center items-center border-[#181D27] bg-[#181D27]"><Heart className="w-[24px] h-[24px] stroke-white fill-none md:stroke-[#961200] md:fill-[#961200]"></Heart></div>
               </div>
             </div>
-            <div className="text-white gap-5 flex mt-5">
+            <div className="text-white gap-10 flex mt-5">
               <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Star className="stroke-[#E4A802] fill-[#E4A802]"/>Rating <span>6.2/10</span> </div>
               <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Video className="stroke-white fill-white"/>Genre <span>Action</span> </div>
               <div className="rounded-2xl flex flex-col gap-2 content-center w-[276px] h-[146px] bg-[#000000] justify-center items-center"> <Image src="/Vector4.png" alt="agelimit" width={26.65} height={26.67}/>Age Limit<span>13</span> </div>       
